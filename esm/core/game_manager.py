eef4b7e1de43df48d87ba37d9aedb12a3aa0e18f
sturdy-robot <pedrenriquegg@hotmail.com>
2022-08-13 22:03:42 -0300

    wip: tentative changes
    * Moving save file responsibility to separate functions
    * The reason behind that is that I don't really see why using that in the GameManager, maybe this can be reused by other modules?
    * If I regret this, I can just revert, but I think it's fine.

--- esm/core/game_manager.py
+++ esm/core/game_manager.py
@@ -40,8 +40,6 @@ class GameManager:
         self.db = db
         self.gamestate = None
         self.current_matches = None
-        self.load = None
-        self.save = None
 
     def get_gamestate(self):
         return GameState(
@@ -55,49 +53,3 @@ class GameManager:
             self.champions.champions_list.copy(),
         )
 
-    def create_save_game(self):
-        self.save = SaveGame(
-            self.get_gamestate(),
-            self.filename,
-            save_directory=self.settings.save_file_dir,
-            autosave_enabled=self.auto_save_enabled,
-        )
-
-    def save_game(self):
-        """
-        Calls the SaveGame module to save the game file.
-        """
-        self.create_save_game()
-        self.save.save_game()
-
-    def auto_save(self):
-        """
-        Calls the SaveGame module to save an autosave file.
-        """
-        self.create_save_game()
-        self.save.save_autosave()
-
-    def get_load_game_files(self):
-        self.load = LoadGame(folder=self.settings.save_file_dir)
-        self.load.get_load_game_files('.cbor')
-
-    def check_if_save_file_exists(self, filename: Union[str, Path]):
-        return os.path.exists(filename)
-
-    def get_autosave_files(self):
-        self.load = LoadGame(folder=self.settings.save_file_dir)
-        self.load.get_load_game_files('.autosav')
-
-    def load_game(self, filename: Union[Path, str]):
-        self.filename = filename
-        self.load = LoadGame(folder=self.settings.save_file_dir)
-        self.gamestate = self.load.load_game_state(filename)
-        self.save = SaveGame(self.gamestate, filename)
-
-    def get_temporary_file(self):
-        self.create_save_game()
-        self.save.save_temporary_file()
-        return self.save.temporary_file
-
-    def delete_temporary_file(self):
-        self.save.delete_temporary_file()

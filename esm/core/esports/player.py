24d9e9a8c93a68f8921498628d17ae7a203e3d0f
Sturdy Robot <pedrenriquegg@hotmail.com>
2023-02-06 21:26:48 -0300

    rewrite players as dataclasses

--- esm/core/esports/player.py
+++ esm/core/esports/player.py
@@ -16,47 +16,22 @@
 import uuid
 from datetime import date, datetime
 from typing import Union
+from dataclasses import dataclass
 
 
+@dataclass
 class Player:
     """
     Base player class.
     May be used by all other eSports.
     """
-
-    def __init__(
-            self,
-            player_id: Union[int, uuid.UUID],
-            nationality: str,
-            first_name: str,
-            last_name: str,
-            birthday: Union[date, datetime, str],
-            nick_name: str,
-    ):
-        if isinstance(player_id, int):
-            self.player_id = uuid.UUID(int=player_id)
-        elif isinstance(player_id, uuid.UUID):
-            self.player_id = player_id
-        # TODO: players should include team's id as well
-
-        self.first_name = first_name
-        self.last_name = last_name
-
-        # This might be necessary if we are using JSON deserialization
-        if isinstance(birthday, str):
-            self.birthday = datetime.strptime(birthday, "%Y/%m/%d").date()
-        elif isinstance(birthday, date):
-            self.birthday = birthday
-        elif isinstance(birthday, datetime):
-            self.birthday = birthday.date()
-
-        self.nick_name = nick_name
-
-        self.nationality = nationality
-
-        # TODO: players should have a "potential" value too. This value tells the game that the player
-        # can improve his overall skill to a certain level
-
+        player_id: uuid.UUID
+        nationality: str
+        first_name: str
+        last_name: str
+        birthday: date
+        nick_name: str
+    
     def get_age(self, today: date = date.today()) -> int:
         """
         Defines the player's age. Today generally refers to the datetime.today function, but when we implement

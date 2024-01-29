c8d6e714194c99c1295b8a5971fdf6b67922dfdd
sturdy-robot <pedrenriquegg@hotmail.com>
2024-01-29 02:00:50 -0300

    chore: update copyright info

--- esm/core/esports/moba/generator/__init__.py
+++ esm/core/esports/moba/generator/__init__.py
@@ -1,5 +1,5 @@
 #      eSports Manager - free and open source eSports Management game
-#      Copyright (C) 2020-2023  Pedrenrique G. Guimarães
+#      Copyright (C) 2020-2024  Pedrenrique G. Guimarães
 #
 #      This program is free software: you can redistribute it and/or modify
 #      it under the terms of the GNU General Public License as published by

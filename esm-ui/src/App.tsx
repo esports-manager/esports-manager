77723b8eeb4e7968c9d7e0a76497393ab0618d09
Sturdy Robot <pedrenriquegg@hotmail.com>
2024-09-10 08:30:26 -0300

    feat(ui): pywebview-based ui (#115)

---
+++ esm-ui/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+      <>
+          <h1 className="text-3xl font-bold underline">
+              Hello world!
+          </h1>
+      </>
+  )
+}
+
+export default App
